cbca5970f8376ed0fee8efe91791f65cf00c9d9b
kingwill101 <hey@glenfordwilliams.com>
2025-10-20 11:05:49 -0500

    Run CI for each package
    Add dartRun helper to create 'gha' and 'gha_cli' jobs. Set working
    directory for analyze, format, and test steps and update the generated
    workflow YAML.

--- .github/workflows/ci.dart
+++ .github/workflows/ci.dart
@@ -1,24 +1,31 @@
 import 'package:gha/gha.dart';
 
 void main() {
+  dartRun(String name) {
+    return Job(
+      name: 'Build and Test ($name)',
+      runsOn: RunnerSpec.single('ubuntu-latest'),
+
+      steps: [
+        checkout(),
+        setupDart(sdk: 'stable'),
+        Step(name: 'Install dependencies', run: 'dart pub get'),
+        Step(name: 'Analyze', run: 'dart analyze', workingDirectory: name),
+        Step(
+          name: 'Format check',
+          run: 'dart format --set-exit-if-changed .',
+          workingDirectory: name,
+        ),
+        Step(name: 'Run tests', run: 'dart test', workingDirectory: name),
+      ],
+    );
+  }
+
   final workflow = Workflow(
     name: 'CI',
     on: WorkflowTriggers.simple(['push', 'pull_request']),
     jobs: {
-      'build': Job(
-        name: 'Build and Test',
-        runsOn: RunnerSpec.single('ubuntu-latest'),
-        steps: [
-          checkout(),
-          setupDart(
-            sdk: 'stable',
-          ),
-          Step(name: 'Install dependencies', run: 'dart pub get'),
-          Step(name: 'Analyze', run: 'dart analyze'),
-          Step(name: 'Format check', run: 'dart format --set-exit-if-changed .'),
-          Step(name: 'Run tests', run: 'dart test'),
-        ],
-      ),
+      for (final name in ['gha', 'gha_cli']) name: dartRun(name),
     },
   );
 
